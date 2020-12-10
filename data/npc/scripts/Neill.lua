50fbbb3d9b4cee25f3cca749380997074ac0eb73
Alexandre Cavalheiro S. Tiago Da Silva <alexandre.cssilva@gmail.com>
2020-12-10 07:17:34 -0300

    refactor: clean neill's code

--- data/npc/scripts/Neill.lua
+++ data/npc/scripts/Neill.lua
@@ -17,84 +17,65 @@ local voices = {
 }
 npcHandler:addModule(VoiceModule:new(voices))
 
-local function creatureSayCallback(cid, type, msg)
+local topics = {
+	[0] = {
+		keywords = {"yes", "festival"},
+		message = {
+			"Indeed! As you willl have to choose your {vocation} soon, this festival will help you decide by showing you each vocation's unique style and techniques. ...",
+			"In each tent, a combat trainer will show you his vocation's special fighting technique. So you can find out which vocation suits you best! ...",
+			"Don't worry, it's all for fun, just to show you the principle, and won't hurt. {OK}?"
+		}
+	},
+	[1] = {
+		keywords = {"yes", "ok"},
+		message = {
+			"Good! Each of the combat and vocation trainers can give you details about his or her vocation, just ask them. ...",
+			"So, shall we {start} your festival tour? Of course, if you already know which vocation to choose, you can also {skip} the tour entirely. What do you say?"
+		}
+	},
+	[2] = {
+		keywords = {"start"},
+		message = {
+			"Alright, here we go! On Rookgaard, you already had a first taste of how we proud knights fight: up close and personal. We're always first in a melee and the strongest heroes in Tibia. ...",
+			"That means, in a fight we go to a monster and hit it upfront with a weapon. That's no job for weaklings, so we knights have a strong defense and many hitpoints to be able to hold out against damage. Are you {ready}?"
+		}
+	},
+	[3] = {
+		keywords = {"ready"},
+		message = "Good! You already fought on Rookgaard, so you probably have a weapon. Or do you need a {sword}?"
+	},
+	[4] = {
+		keywords = {"yes", "sword"},
+		message = {
+			"Here's a training sword. Some last hints - knights fight with a shield and weapon. Remember: we fight at the front, so we need good armor. We're tough, but we don't fight from a distance or heal others. Always have a health potion ready! ...",
+			"if you need a shield, take one from the box, left of the entrance. It will hold off two enemies at once. ...",
+			"Have fun and make your way through this tent, past the skeletons. The next tent's just north of this one. I hear they fight from a little distance in there. Huh. Well."
+		},
+		func = function(cid)
+			local player = Player(cid)
+
+			player:setStorageValue(storage, 1)
+			player:addItem(ItemType("mean knight sword"):getId())
+		end
+	}
+}
+
+local function creatureSayCallback(cid, talkType, msg)
 	if not npcHandler:isFocused(cid) then
 		return false
 	end
 
-	if npcHandler.topic[cid] == 0 then
-		if not msgcontains(msg, "yes") and not msgcontains(msg, "festival") then
-			return false
-		end
+	local topic = topics[npcHandler.topic[cid]]
 
-		npcHandler:say({
-			"Indeed! As you willl have to choose your {vocation} soon, this festival will help you decide by showing you each vocation's unique style and techniques. ...",
-			"In each tent, a combat trainer will show you his vocation's special fighting technique. So you can find out which vocation suits you best! ...",
-			"Don't worry, it's all for fun, just to show you the principle, and won't hurt. {OK}?"
-		}, cid)
-		npcHandler.topic[cid] = 1
+	if not isInArray(topic.keywords, msg:lower()) then
 		return false
 	end
 
-	if npcHandler.topic[cid] == 1 then
-		if not msgcontains(msg, "yes") and not msgcontains(msg, "ok") then
-			return false
-		end
+	npcHandler:say(topic.message, cid)
+	npcHandler.topic[cid] = npcHandler.topic[cid] + 1
 
-		npcHandler:say({
-			"Good! Each of the combat and vocation trainers can give you details about his or her vocation, just ask them. ...",
-			"So, shall we {start} your festival tour? Of course, if you already know which vocation to choose, you can also {skip} the tour entirely. What do you say?"
-		}, cid)
-
-		npcHandler.topic[cid] = 2
-		return false
-
-	end
-
-	if npcHandler.topic[cid] == 2 then
-		if not msgcontains(msg, "start") then
-			return false
-		end
-
-		npcHandler:say({
-			"Alright, here we go! On Rookgaard, you already had a first taste of how we proud knights fight: up close and personal. We're always first in a melee and the strongest heroes in Tibia. ...",
-			"That means, in a fight we go to a monster and hit it upfront with a weapon. That's no job for weaklings, so we knights have a strong defense and many hitpoints to be able to hold out against damage. Are you {ready}?"
-		}, cid)
-
-		npcHandler.topic[cid] = 3
-		return false
-
-	end
-
-	if npcHandler.topic[cid] == 3 then
-		if not msgcontains(msg, "ready") then
-			return false
-		end
-
-		npcHandler:say("Good! You already fought on Rookgaard, so you probably have a weapon. Or do you need a {sword}?", cid)
-
-		npcHandler.topic[cid] = 4
-		return false
-
-	end
-
-	if npcHandler.topic[cid] == 4 then
-		if not msgcontains(msg, "yes") and not msgcontains(msg, "sword") then
-			return false
-		end
-
-		npcHandler:say({
-			"Here's a training sword. Some last hints - knights fight with a shield and weapon. Remember: we fight at the front, so we need good armor. We're tough, but we don't fight from a distance or heal others. Always have a health potion ready! ...",
-			"if you need a shield, take one from the box, left of the entrance. It will hold off two enemies at once. ...",
-			"Have fun and make your way through this tent, past the skeletons. The next tent's just north of this one. I hear they fight from a little distance in there. Huh. Well."
-		}, cid)
-
-		local player = Player(cid)
-
-		player:setStorageValue(storage, 1)
-		player:addItem(ItemType("mean knight sword"):getId())
-		return false
-
+	if type(topic.func) == "function" then
+		topic.func(cid)
 	end
 
 	return true
